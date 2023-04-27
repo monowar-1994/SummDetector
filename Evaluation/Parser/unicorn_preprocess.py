2b7fcb6d4131122198b5010f023c0d9bc07d29dc
Jinyuan Liang <89882411+JyL1ang@users.noreply.github.com>
2023-04-27 11:08:16 -0700

    Update the preprocess codes. It is ugly version, needed to be clean in the future.

--- Evaluation/Parser/unicorn_preprocess.py
+++ Evaluation/Parser/unicorn_preprocess.py
@@ -59,7 +59,7 @@ def get_decoded_path(base64_encoded_path):
     return a.decode('ascii')
 
 
-############################################## Training set #############################################
+
 train_graph_0_path = "/scratch/cadet_graphs_attr/Training_Graphs/graph_0.edgelist"
 train_graph_1_path = "/scratch/cadet_graphs_attr/Training_Graphs/graph_1.edgelist"
 train_graph_2_path = "/scratch/cadet_graphs_attr/Training_Graphs/graph_2.edgelist"
@@ -70,53 +70,9 @@ train_graph_1 = nx.MultiDiGraph(nx.read_edgelist(train_graph_1_path))
 train_graph_2 = nx.MultiDiGraph(nx.read_edgelist(train_graph_2_path))
 
 train_graph = nx.compose_all([train_graph_0, train_graph_1, train_graph_2])
-sorted_train_edges = sorted(train_graph.edges(data=True, keys=True), key=lambda x: x[3].get('timestamp'))
 
-i = 0
-file_no = 0
-batch_size = int(len(sorted_train_edges) / 100)
-logical_time = 0
 
-file = open("./train/benign_{}.csv".format(file_no), 'w')
-for edge in sorted_train_edges:
-    src_id = edge[0]
-    dst_id = edge[1]
-    src_type = str(mapping_dict[edge[3]['source_type']])
-    src_label = src_type + "_" + get_subject_label(uuid_exec_maps, index_uuid_maps, src_id)
-    hashed_src_label = hashgen(src_label)
 
-    dst_type = str(mapping_dict[edge[3]['dest_type']])
-    if "File" in dst_type:
-        file_object_path = edge[3]['attr']
-        if file_object_path:
-            dst_label = dst_type + "_" + get_decoded_path(file_object_path)
-        else:
-            dst_label = dst_type + "_" + "null"
-    elif "Netflow" in dst_type:
-        ip_addr = edge[3]['attr'].replace(":", "_")
-        dst_label = dst_type + "_" + ip_addr
-    elif "Subject" in dst_type:
-        exec_name = get_subject_label(uuid_exec_maps, index_uuid_maps, dst_id)
-        dst_label = dst_type + "_" + exec_name
-    else:
-        dst_label = dst_type + "_" + "null"
-
-    hashed_dst_label = hashgen(dst_label)
-    edge_type = edge[3]['event_type'].encode("utf-8")
-    hashed_edge_type = hashgen(edge_type)
-
-    file.write("{} {} {}:{}:{}:{}\n".format(src_id, dst_id, hashed_src_label, hashed_dst_label, hashed_edge_type,
-                                            logical_time))
-    logical_time += 1
-    i += 1
-    if i == batch_size:
-        file.close()
-        file_no += 1
-        file = open("./train/benign_{}.csv".format(file_no), 'w')
-        i = 0
-file.close()
-
-############################################## Testing set #############################################
 test_graph_0_path = "/scratch/cadet_graphs_attr/Testing_Graphs/graph_0.edgelist"
 test_graph_1_path = "/scratch/cadet_graphs_attr/Testing_Graphs/graph_1.edgelist"
 test_graph_2_path = "/scratch/cadet_graphs_attr/Testing_Graphs/graph_2.edgelist"
@@ -139,6 +95,7 @@ test_graph_7 = nx.MultiDiGraph(nx.read_edgelist(test_graph_7_path))
 test_graph_8 = nx.MultiDiGraph(nx.read_edgelist(test_graph_8_path))
 
 test_graph = nx.compose_all([
+    train_graph,
     test_graph_0,
     test_graph_1,
     test_graph_2,
@@ -152,32 +109,80 @@ test_graph = nx.compose_all([
 sorted_test_edges = sorted(test_graph.edges(data=True, keys=True), key=lambda x: x[3].get('timestamp'))
 base_timestamp = 1522706861813350340
 
-batch_size = int(len(sorted_test_edges) / 100)
-file_names = []
+attack_edges = [[],[],[],[]]
+benign_edges = []
+
+for edge in tqdm(sorted_test_edges):
+    edge_time = edge[3]['timestamp'] + base_timestamp
+    if edge_time >= 1523028000000000000 and edge_time <= 1523030940000000000:
+        attack_edges[0].append(edge)
+    elif edge_time >= 1523473620000000000 and edge_time <= 1523474160000000000:
+        attack_edges[1].append(edge)
+    elif edge_time >= 1523555940000000000 and edge_time <= 1523558340000000000:
+        attack_edges[2].append(edge)
+    elif edge_time >= 1523624580000000000 and edge_time <= 1523625540000000000:
+        attack_edges[3].append(edge)
+    else:
+        benign_edges.append(edge)
+     
+    
+i = 0
+file_no = 0
+batch_size = int(len(benign_edges) / 50)
 logical_time = 0
 
-benign_file_no = 0
-attack_file_no = 0
-for i in range(0, len(sorted_test_edges), batch_size):
-    batched_edges = sorted_test_edges[i:i + batch_size]
-    is_attack = False
-    for edge in tqdm(batched_edges, desc="Batch {}".format(benign_file_no + attack_file_no)):
-        edge_time = edge[3]['timestamp'] + base_timestamp
-        for start_time, stop_time in attack_durations:
-            if edge_time >= start_time and edge_time <= stop_time:
-                is_attack = True
-                break
-    if is_attack:
-        file_names.append("./test/attack_{}.csv".format(attack_file_no))
-        attack_file_no += 1
-    else:
-        file_names.append("./test/bengin_{}.csv".format(benign_file_no))
-        benign_file_no += 1
+file = open("../Evaluation/Parser/test/benign_{}.csv".format(file_no), 'w')
+for edge in benign_edges:
+    src_id = edge[0]
+    dst_id = edge[1]
+    
+    if src_id == dst_id:
+        continue
+    
+    src_type = str(mapping_dict[edge[3]['source_type']])
+    src_label = src_type + "_" + get_subject_label(uuid_exec_maps, index_uuid_maps, src_id)
+    hashed_src_label = hashgen(src_label)
 
-    file = open(file_names[-1], 'w')
-    for edge in batched_edges:
+    dst_type = str(mapping_dict[edge[3]['dest_type']])
+    if "File" in dst_type:
+        file_object_path = edge[3]['attr']
+        if file_object_path:
+            dst_label = dst_type + "_" + get_decoded_path(file_object_path)
+        else:
+            dst_label = dst_type + "_" + "null"
+    elif "Netflow" in dst_type:
+        ip_addr = edge[3]['attr'].replace(":","_")
+        dst_label = dst_type + "_" + ip_addr
+    elif "Subject" in dst_type:
+        exec_name = get_subject_label(uuid_exec_maps, index_uuid_maps, dst_id)
+        dst_label = dst_type + "_" + exec_name
+    else:
+        dst_label = dst_type + "_" + "null"
+
+    hashed_dst_label = hashgen(dst_label)
+    edge_type = edge[3]['event_type'].encode("utf-8")
+    hashed_edge_type = hashgen(edge_type)
+    
+    file.write("{}\t{}\t{}:{}:{}:{}\n".format(src_id, dst_id, hashed_src_label, hashed_dst_label, hashed_edge_type, logical_time))
+    logical_time += 1
+    i+=1
+    if i==batch_size:
+        file.close()
+        file_no += 1
+        file = open("../Evaluation/Parser/test/benign_{}.csv".format(file_no), 'w')
+        i = 0
+file.close()
+
+logical_time = 0
+for i in range(4):
+    file = open("../Evaluation/Parser/test/attack_{}.csv".format(i), 'w')
+    for edge in tqdm(attack_edges[i]):
         src_id = edge[0]
         dst_id = edge[1]
+
+        if src_id == dst_id:
+            continue
+
         src_type = str(mapping_dict[edge[3]['source_type']])
         src_label = src_type + "_" + get_subject_label(uuid_exec_maps, index_uuid_maps, src_id)
         hashed_src_label = hashgen(src_label)
@@ -190,7 +195,7 @@ for i in range(0, len(sorted_test_edges), batch_size):
             else:
                 dst_label = dst_type + "_" + "null"
         elif "Netflow" in dst_type:
-            ip_addr = edge[3]['attr'].replace(":", "_")
+            ip_addr = edge[3]['attr'].replace(":","_")
             dst_label = dst_type + "_" + ip_addr
         elif "Subject" in dst_type:
             exec_name = get_subject_label(uuid_exec_maps, index_uuid_maps, dst_id)
@@ -202,7 +207,11 @@ for i in range(0, len(sorted_test_edges), batch_size):
         edge_type = edge[3]['event_type'].encode("utf-8")
         hashed_edge_type = hashgen(edge_type)
 
-        file.write("{} {} {}:{}:{}:{}\n".format(src_id, dst_id, hashed_src_label, hashed_dst_label, hashed_edge_type,
-                                                logical_time))
+        file.write("{}\t{}\t{}:{}:{}:{}\n".format(src_id, dst_id, hashed_src_label, hashed_dst_label, hashed_edge_type, logical_time))
         logical_time += 1
+        if i==batch_size:
+            file.close()
+            file = open("../Evaluation/Parser/test/attack_{}.csv".format(i), 'w')
     file.close()
+
+
